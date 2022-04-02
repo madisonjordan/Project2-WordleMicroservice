c1ff002ecfeaf46e571a35ec4ca70b7d1728d796
Madison Jordan <madisonjordan@users.noreply.github.com>
2022-04-02 13:30:43 -0700

    add answer checking function logic (#16)
    start implementation of answer checking function

---
+++ answer-check.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+guess = 'zones'
+answer = 'zebra'
+
+for x in range(len(answer)):
+    if (f"{answer}"[x] == f"{guess}"[x]):
+        print(f"{guess}"[x], " - correct position")
+    elif (f"{guess}"[x] in f"{answer}"):
+        print(f"{guess}"[x], " - exists")
+    else:
+        print(f"{guess}"[x], " - not found")
+
