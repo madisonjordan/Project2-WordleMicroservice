00ced7d71668305ec77a1a80f8a85c00b989df18
Madison J <madijrdn@gmail.com>
2022-04-02 17:48:38 -0700

    add answers database creation script

---
+++ bin/answers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# load json file using python
+PARSE=`python - <<'EOF'
+import json;
+file = open('../share/answers.json');
+answers = json.load(file);
+file.close();
+for i in answers:
+    print(i);
+EOF`
+
+# populate database
+DB='answers.db'
+
+if [ ! -f "../var/$DB" ]
+then
+    echo ${PARSE} | sqlite-utils insert ../var/$DB answers - \
+    --text --convert '({"word": w} for w in text.split())' --pk=word 
+else
+    echo "$DB already exists"
+fi
