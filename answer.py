4744920ea5a2e888edb62fe4ec8bad1d91e9bd54
Madison J <madijrdn@gmail.com>
2022-04-05 21:20:24 -0700

    update readme and procfile

--- answer.py
+++ answer.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-guess = 'zones'
-answer = 'zebra'
+guess = "zones"
+answer = "zebra"
 
 for x in range(len(answer)):
-    if (f"{answer}"[x] == f"{guess}"[x]):
+    if f"{answer}"[x] == f"{guess}"[x]:
         print(f"{guess}"[x], " - correct position")
-    elif (f"{guess}"[x] in f"{answer}"):
+    elif f"{guess}"[x] in f"{answer}":
         print(f"{guess}"[x], " - exists")
     else:
         print(f"{guess}"[x], " - not found")
