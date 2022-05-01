cc43eb73290825a9ce77ebb5e9d334b690092917
Madison J <madijrdn@gmail.com>
2022-05-01 15:48:12 -0700

    add wins to getTop10

--- bin/getTop10.py
+++ bin/getTop10.py
@@ -6,7 +6,7 @@ import itertools
 shards = 3
 database_dir = "./var/"
 
-
+# print Streaks - Top10 from each shard
 def top10_streaks_all_time():
     shard_top10 = collections.defaultdict(list)
     temp = []
@@ -23,4 +23,23 @@ def top10_streaks_all_time():
         print("zadd", "streaks", all_list[user][2], '"' + all_list[user][1] + '"')
 
 
+# print Wins - Top10 from each shard
+def top10_wins():
+    shard_top10 = collections.defaultdict(list)
+    temp = []
+    all_list = []
+    top10 = []
+    for shard in range(shards):
+        db = sqlite3.connect(f"{database_dir}stats{shard}.db")
+        shard_top10[shard] = db.execute(
+            "SELECT wins.user_id, users.username, wins.wins FROM wins INNER JOIN users ON wins.user_id=users.user_id ORDER BY wins DESC LIMIT 10"
+        ).fetchall()
+        temp.append(shard_top10[shard])
+    all_list = list(itertools.chain(*temp))
+    for user in range(30):
+        print("zadd", "wins", all_list[user][2], '"' + all_list[user][1] + '"')
+
+
+# output for redis
 top10_streaks_all_time()
+top10_wins()
