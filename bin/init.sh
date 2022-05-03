2bb4207adc9688b26209fffede13256c02382577
Madison J <madijrdn@gmail.com>
2022-05-02 19:37:55 -0700

    fix cronjob to use path env variables

--- bin/init.sh
+++ bin/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "export PROJ_PATH=$(pwd)" >> ~/.bash_profile
+echo "export PROJ_PATH=$(pwd)" >> ~/.bash_profile && . ~/.bash_profile
 
 #################
 #   words.db    #
--- bin/leaderboard.sh
+++ bin/leaderboard.sh
@@ -1,2 +1,3 @@
+. ~/.bash_profile && \
 echo "Update Leaderboard: $(date)" && \
 $PROJ_PATH/dist/getTop10 | redis-cli --pipe 
