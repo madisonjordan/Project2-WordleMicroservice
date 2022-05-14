08c3f0ff35b0309e7d6a221d1ae5c645d4910604
Madison J <madijrdn@gmail.com>
2022-05-14 05:09:34 +0000

    add structure for bff service

--- api/bff.py
+++ api/bff.py
@@ -2,20 +2,67 @@ import asyncio
 import httpx
 import json
 
-from answers import app as answers
+from answers import app as answer_service
+from words import app as word_service
+from stats import app as stats_service
+from game_state import app as game_state_service
 
 
+# TODO: check if user has guesses remaining in this game
+async def isValidGame():
+    async with httpx.AsyncClient(
+        app=game_state_service, base_url="http://120.0.0.1:9999"
+    ) as client:
+        r = await client.get("/game/")
+        print(r.status_code)
+        print(json.dumps(json.loads(r.text), indent=4))
+
+
+# TODO: check if word is valid guess
+async def isValidWord():
+    async with httpx.AsyncClient(
+        app=word_service, base_url="http://120.0.0.1:9999"
+    ) as client:
+        r = await client.get("/word/guess")
+        print(r.status_code)
+        print(json.dumps(json.loads(r.text), indent=4))
+
+
+# TODO: check if valid guess is correct answer
 async def check_guess():
     async with httpx.AsyncClient(
-        app=answers, base_url="http://120.0.0.1:9999"
+        app=answer_service, base_url="http://120.0.0.1:9999"
     ) as client:
         r = await client.get("/check/soaps")
         print(r.status_code)
         print(json.dumps(json.loads(r.text), indent=4))
 
 
+# TODO: update user stats if this game is finished
+async def updateStats():
+    async with httpx.AsyncClient(
+        app=stats_service, base_url="http://120.0.0.1:9999"
+    ) as client:
+        r = await client.post("/users/christina22")
+        print(r.status_code)
+        print(json.dumps(json.loads(r.text), indent=4))
+
+
+# TODO: return user stats
+async def getStats():
+    async with httpx.AsyncClient(
+        app=stats_service, base_url="http://120.0.0.1:9999"
+    ) as client:
+        r = await client.get("/users/christina22")
+        print(r.status_code)
+        print(json.dumps(json.loads(r.text), indent=4))
+
+
+# define tasks to run concurrently
 # async def main():
 
-
+# run concurrently
 # asyncio.run(main())
+
+# test async
 asyncio.run(check_guess())
